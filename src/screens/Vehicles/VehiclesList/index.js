1a53a9217995203f86a6434a62698656308dd9b4
Rafael Barbosa <rafaelbarbosamac@gmail.com>
2021-06-25 18:37:16 -0300

    Add delete, change and add vehicle buttons

--- src/screens/Vehicles/VehiclesList/index.js
+++ src/screens/Vehicles/VehiclesList/index.js
@@ -1,14 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo, useContext } from 'react';
+import { Link } from 'react-router-dom';
 
 import { DataGrid } from '@material-ui/data-grid';
+import { Button } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
 
 import columns from './columns';
 import VehicleService from '../services';
 import vehicleParser from './vehicleParser';
+import Authentication from '../../../contexts/authentication';
 
 const VehiclesList = () => {
+  const { isLoggedIn } = useContext(Authentication);
   const [vehicles, setVehicles] = useState([]);
+  const [selectedVehicle, setSelectedVehicle] = useState({});
   const [isLoading, setIsLoading] = useState(false);
+  const shouldDisableButtons = useMemo(() =>
+    selectedVehicle && Object.keys(selectedVehicle).length === 0,
+  [selectedVehicle]);
 
   const loadVehicles = () => {
     VehicleService.getAll()
@@ -20,21 +29,54 @@ const VehiclesList = () => {
       });
   };
 
+  const deleteVehicle = () => {
+    VehicleService.delete(selectedVehicle);
+  };
+
   useEffect(() => {
     setIsLoading(true);
     loadVehicles();
   }, []);
 
   return (
-    <DataGrid
-      autoHeight
-      columns={columns}
-      rows={vehicles}
-      disableColumnMenu
-      disableColumnSelector
-      disableDensitySelector
-      loading={isLoading}
-    />
+    <>
+      <DataGrid
+        columns={columns}
+        rows={vehicles}
+        loading={isLoading}
+        onRowSelected={(gridSelection) => setSelectedVehicle(gridSelection.data)}
+        autoHeight
+        disableColumnMenu
+        disableColumnSelector
+        disableDensitySelector
+      />
+      {isLoggedIn &&
+        <Box display="flex" justifyContent="flex-end" mt={2}>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={deleteVehicle}
+            disabled={shouldDisableButtons}
+          >
+            Excluir
+          </Button>
+          <Box mx={1}>
+            <Button
+              to={`alteracao-veiculo/${selectedVehicle.id}`}
+              component={Link}
+              variant="contained"
+              color="primary"
+              disabled={shouldDisableButtons}
+            >
+              Alterar
+            </Button>
+          </Box>
+          <Button to="/cadastro-veiculo" component={Link} variant="contained" color="primary">
+            Incluir
+          </Button>
+        </Box>
+      }
+    </>
   );
 };
 
--- src/screens/Vehicles/VehiclesList/tests/index.test.js
+++ src/screens/Vehicles/VehiclesList/tests/index.test.js
@@ -1,17 +1,113 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import VehiclesList from '..';
 import VehicleService from '../../services';
 import mockedVehicles from './mockedVehicles';
+import Authentication from '../../../../contexts/authentication';
 
 jest.mock('../../services');
 
 describe('<VehiclesList />', () => {
-  it('should render the vehicles list correctly', async () => {
-    VehicleService.getAll.mockImplementationOnce(() => Promise.resolve(mockedVehicles));
-    render(<VehiclesList />);
+  describe('rendering', () => {
+    it('should render the VehiclesList component correctly', async () => {
+      const mockedState = {
+        isLoggedIn: true,
+      };
+      VehicleService.getAll.mockImplementationOnce(() => Promise.resolve(mockedVehicles));
+      render(
+        <Authentication.Provider value={mockedState}>
+          <MemoryRouter>
+            <VehiclesList />
+          </MemoryRouter>
+        </Authentication.Provider>,
+      );
+      await waitFor(() =>
+        expect(screen.getAllByRole('row')).toHaveLength(6),
+        expect(screen.getByRole('button', { name: 'Excluir' })).toBeInTheDocument(),
+        expect(screen.getByRole('button', { name: 'Alterar' })).toBeInTheDocument(),
+        expect(screen.getByRole('button', { name: 'Incluir' })).toBeInTheDocument(),
+      );
+    });
 
-    await waitFor(() => expect(screen.getAllByRole('row')).toHaveLength(6));
+    it('should not render the buttons if the user is not logged in', async () => {
+      const mockedState = {
+        isLoggedIn: false,
+      };
+      VehicleService.getAll.mockImplementationOnce(() => Promise.resolve(mockedVehicles));
+      render(
+        <Authentication.Provider value={mockedState}>
+          <MemoryRouter>
+            <VehiclesList />
+          </MemoryRouter>
+        </Authentication.Provider>,
+      );
+
+      await waitFor(
+        () => expect(screen.queryAllByText('Excluir')).toHaveLength(0),
+        expect(screen.queryAllByText('Alterar')).toHaveLength(0),
+        expect(screen.queryAllByText('Incluir')).toHaveLength(0),
+      );
+    });
+  });
+
+  describe('button enabling', () => {
+    it('should enable the buttons after a row is clicked', async () => {
+      const mockedState = {
+        isLoggedIn: true,
+      };
+      VehicleService.getAll.mockImplementationOnce(() => Promise.resolve(mockedVehicles));
+      render(
+        <Authentication.Provider value={mockedState}>
+          <MemoryRouter>
+            <VehiclesList />
+          </MemoryRouter>
+        </Authentication.Provider>,
+      );
+
+      await waitFor(() =>
+        expect(screen.getByRole('button', { name: 'Excluir' })).toBeDisabled(),
+        expect(
+          screen.getByRole('button', { name: 'Alterar' }).attributes.getNamedItem('aria-disabled'),
+        ).toBeTruthy(),
+      );
+
+      const firstVehicleRow = screen.getAllByRole('row')[1];
+      fireEvent.click(firstVehicleRow);
+
+      await waitFor(() =>
+        expect(screen.getByRole('button', { name: 'Excluir' })).toBeEnabled(),
+        expect(
+          screen.getByRole('button', { name: 'Alterar' })
+        ).toHaveAttribute('aria-disabled', 'false'),
+      );
+    });
+  });
+
+  describe('button functioning', () => {
+    it('should enable the buttons after a row is clicked', async () => {
+      const mockedState = {
+        isLoggedIn: true,
+      };
+      VehicleService.getAll.mockImplementationOnce(() => Promise.resolve(mockedVehicles));
+      VehicleService.delete.mockImplementation(() => {});
+      render(
+        <Authentication.Provider value={mockedState}>
+          <MemoryRouter>
+            <VehiclesList />
+          </MemoryRouter>
+        </Authentication.Provider>,
+      );
+
+      await waitFor(() => expect(screen.getAllByRole('row')).toHaveLength(6));
+
+      const firstVehicleRow = screen.getAllByRole('row')[1];
+      const deleteButton = screen.getByRole('button', { name: 'Excluir' });
+      fireEvent.click(firstVehicleRow);
+      fireEvent.click(deleteButton);
+
+      await waitFor(() => expect(VehicleService.delete).toHaveBeenCalled());
+    });
   });
 });
