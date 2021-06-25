17dfb1afd5c4af836b44f47dee8bc6377a2ed91e
Rafael Barbosa <rafaelbarbosamac@gmail.com>
2021-06-25 18:36:32 -0300

    Add fake delete service

--- src/screens/Vehicles/services/index.js
+++ src/screens/Vehicles/services/index.js
@@ -2,6 +2,9 @@ const VehicleService = {
   async getAll() {
     return fetch('https://carango-bom-api.herokuapp.com/veiculos').then((r) => r.json());
   },
+  delete() {
+    return true;
+  }
 };
 
 export default VehicleService;
