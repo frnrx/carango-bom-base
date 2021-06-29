c6565c7c52be1888d07be5799e72d4bfe95213cd
g.froener <gabriela.froener@gmail.com>
2021-06-29 10:35:15 -0300

    Removing BrandsList change for now to avoid bug on sonar

--- src/screens/Brands/BrandsList/index.js
+++ src/screens/Brands/BrandsList/index.js
@@ -20,9 +20,9 @@ const BrandsList = () => {
   };
 
   const change = () => {
-    if (selectedBrand && selectedBrand?.id) {
-      history.push(`/alteracao-marca/${selectedBrand?.id}`);
-    }
+    // if (selectedBrand && selectedBrand?.id) {
+    //   history.push(`/alteracao-marca/${selectedBrand?.id}`);
+    // }
   };
 
   const remove = () => {
