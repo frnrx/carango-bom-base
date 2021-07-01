917b11a03d0939c278e5a48031a83bdb3ae2a185
Rafael Barbosa <rafaelbarbosamac@gmail.com>
2021-07-01 16:48:19 -0300

    Fix vehicleParser test

--- src/screens/Vehicles/VehiclesList/tests/vehicleParser.test.js
+++ src/screens/Vehicles/VehiclesList/tests/vehicleParser.test.js
@@ -1,8 +1,8 @@
 import vehicleParser from '../vehicleParser';
 
 const mockedVehiclesServiceGetAllReturn = [
-  { id: 134, modelo: 'KA 123456', ano: 2021, valor: 80000.0, marca: { id: 34, nome: 'FORD' } },
-  { id: 154, modelo: 'teste', ano: 2000, valor: 10000.0, marca: { id: 34, nome: 'FIAT' } },
+  { id: 134, modelo: 'KA 123456', ano: 2021, preco: 80000.0, marca: { id: 34, nome: 'FORD' } },
+  { id: 154, modelo: 'teste', ano: 2000, preco: 10000.0, marca: { id: 34, nome: 'FIAT' } },
 ];
 
 describe('vehicleParser', () => {
