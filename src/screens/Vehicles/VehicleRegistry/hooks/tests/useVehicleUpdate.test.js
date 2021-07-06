343f3c48106abebb4bf4f3933f4b509eac18c572
Rafael Barbosa <rafaelbarbosamac@gmail.com>
2021-07-06 15:30:16 -0300

    Handle getVehicle at useVehicleUpdate

--- src/screens/Vehicles/VehicleRegistry/hooks/tests/useVehicleUpdate.test.js
+++ src/screens/Vehicles/VehicleRegistry/hooks/tests/useVehicleUpdate.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { renderHook, act } from '@testing-library/react-hooks';
 import useVehicleUpdate from '../useVehicleUpdate';
-import { updateVehicle } from '../../../services';
+import { updateVehicle, getVehicle } from '../../../services';
 import { SnackBarContext } from '../../../../../contexts/snackbar';
 import { AuthenticationContext } from '../../../../../contexts/authentication';
 
@@ -45,7 +45,7 @@ describe('useVehicleUpdate', () => {
       }
     );
     
-    it('should set isLoading to true at the start of the updateVehicle and to false after that',
+    it('should set isUpdateLoading to true at start of the updateVehicle and to false after that',
       async () => {
         updateVehicle.mockImplementationOnce(() => Promise.resolve());
   
@@ -53,9 +53,9 @@ describe('useVehicleUpdate', () => {
         act(() => {
           result.current.update();
         });
-        expect(result.current.isLoading).toBeTruthy();
+        expect(result.current.isUpdateLoading).toBeTruthy();
         await waitForNextUpdate();
-        expect(result.current.isLoading).toBeFalsy();
+        expect(result.current.isUpdateLoading).toBeFalsy();
       }
     );
   
@@ -72,6 +72,70 @@ describe('useVehicleUpdate', () => {
         content: 'Erro inesperado ao atualizar veículo!',
         customSeverity: 'error',
       });
+    });
+  });
+
+  describe('getVehicle handling', () => {
+    it('should not call the getVehicle service when the vehicleId is undefined', () => {
+      getVehicle.mockImplementationOnce(() => jest.fn());
+  
+      renderHook(() => useVehicleUpdate(), { wrapper });
+      expect(getVehicle).not.toHaveBeenCalled();
+    });
+
+    it('should call the addAlert function when the getAllVehicles returns an error', async () => {
+      const mockedServiceReturn = { error: 'error' };
+      getVehicle.mockImplementationOnce(() => Promise.reject(mockedServiceReturn));
+  
+      const { waitForNextUpdate } = renderHook(() => useVehicleUpdate(2), { wrapper });
+      await waitForNextUpdate();
+      expect(mockedSnackbarValue.addAlert).toHaveBeenCalledWith({
+        content: 'Erro inesperado ao carregar informações do veículo!',
+        customSeverity: 'error',
+      });
+      expect(mockHistoryPush).toHaveBeenCalledWith('/');
+    });
+
+    it('should set isGetLoading to true at start of the getVehicle and to false after that',
+      async () => {
+        const mockedServiceReturn = { error: 'error' };
+        getVehicle.mockImplementationOnce(() => Promise.reject(mockedServiceReturn));
+  
+        const { result, waitForNextUpdate } = renderHook(() => useVehicleUpdate(2), { wrapper });
+        expect(result.current.isGetLoading).toBeTruthy();
+        await waitForNextUpdate();
+        expect(result.current.isGetLoading).toBeFalsy();
+      }
+    );
+
+    it('should call all the setters correctly after the return of the endpoint', async () => {
+      const mockedVehicleId = 3;
+      const mockedServiceReturn = { 
+        id: mockedVehicleId,
+        modelo: 'fake model',
+        ano: 2010,
+        preco: 7000,
+        marca: {
+          id: 3,
+          nome: 'Fiat'
+        },
+      };
+      const mockedSetters = {
+        setBrand: jest.fn(),
+        setModel: jest.fn(),
+        setYear: jest.fn(),
+        setValue: jest.fn(),
+      };
+      getVehicle.mockImplementationOnce(() => Promise.resolve(mockedServiceReturn));
+  
+      const { waitForNextUpdate } = renderHook(() =>
+        useVehicleUpdate(mockedVehicleId, mockedSetters), { wrapper }
+      );
+      await waitForNextUpdate();
+      expect(mockedSetters.setBrand).toHaveBeenCalledWith(mockedServiceReturn.marca);
+      expect(mockedSetters.setModel).toHaveBeenCalledWith(mockedServiceReturn.modelo);
+      expect(mockedSetters.setYear).toHaveBeenCalledWith(mockedServiceReturn.ano);
+      expect(mockedSetters.setValue).toHaveBeenCalledWith(mockedServiceReturn.preco);
     });
   });
 });
--- src/screens/Vehicles/VehicleRegistry/hooks/useVehicleUpdate.js
+++ src/screens/Vehicles/VehicleRegistry/hooks/useVehicleUpdate.js
@@ -1,20 +1,21 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { SnackBarContext } from '../../../../contexts/snackbar';
 import { AuthenticationContext } from '../../../../contexts/authentication';
 
-import { updateVehicle } from '../../services';
+import { updateVehicle, getVehicle } from '../../services';
 
-const useVehicleUpdate = () => {
-  const [isLoading, setIsLoading] = useState(false);
+const useVehicleUpdate = (vehicleId, setters) => {
+  const [isUpdateLoading, setIsUpdateLoading] = useState(false);
+  const [isGetLoading, setIsGetLoading] = useState(false);
   const { addAlert } = useContext(SnackBarContext);
   const { userJWT } = useContext(AuthenticationContext);
   const history = useHistory();
 
-  const update = (vehicleId, brand, model, year, value) => {
-    setIsLoading(true);
-    updateVehicle(userJWT, vehicleId, brand, model, year, value)
+  const update = (id, brand, model, year, value) => {
+    setIsUpdateLoading(true);
+    updateVehicle(userJWT, id, brand, model, year, value)
       .then(() => {
         addAlert({ content: 'Veículo atualizado corretamente!', customSeverity: 'success' });
         history.push('/');
@@ -23,11 +24,41 @@ const useVehicleUpdate = () => {
         addAlert({ content: 'Erro inesperado ao atualizar veículo!', customSeverity: 'error' });
       })
       .finally(() => {
-        setIsLoading(false);
+        setIsUpdateLoading(false);
       });
   };
 
-  return { update, isLoading };
+  const loadVehicle = (id) => {
+    setIsGetLoading(true);
+    getVehicle(id, userJWT)
+      .then((data) => {
+        setters.setBrand({
+          id: data.marca.id,
+          name: data.marca.nome,
+        });
+        setters.setModel(data.modelo);
+        setters.setYear(data.ano);
+        setters.setValue(data.preco);
+      })
+      .catch(() => {
+        addAlert({
+          content: 'Erro inesperado ao carregar informações do veículo!',
+          customSeverity: 'error',
+        });
+        history.push('/');
+      })
+      .finally(() => {
+        setIsGetLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    if (vehicleId) {
+      loadVehicle(vehicleId);
+    }
+  }, []);
+
+  return { update, isUpdateLoading, isGetLoading };
 };
 
 export default useVehicleUpdate;
