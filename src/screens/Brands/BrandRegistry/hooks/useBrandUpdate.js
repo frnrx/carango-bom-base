ec311edd6a784c13f2eef0f1da2409acdffa01d1
Rafael Barbosa <rafaelbarbosamac@gmail.com>
2021-07-07 17:49:39 -0300

    Create useBrandUpdate and useBrandRegistry to handle new services

---
+++ src/screens/Brands/BrandRegistry/hooks/useBrandUpdate.js
@@ -0,0 +1,58 @@
+import { useState, useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { SnackBarContext } from '../../../../contexts/snackbar';
+import { AuthenticationContext } from '../../../../contexts/authentication';
+
+import { updateBrand, getBrand } from '../../services';
+
+const useBrandUpdate = (brandId, setBrand) => {
+  const [isUpdateLoading, setIsUpdateLoading] = useState(false);
+  const [isGetLoading, setIsGetLoading] = useState(false);
+  const { addAlert } = useContext(SnackBarContext);
+  const { userJWT } = useContext(AuthenticationContext);
+  const history = useHistory();
+
+  const update = (brand) => {
+    setIsUpdateLoading(true);
+    updateBrand(userJWT, brandId, brand)
+      .then(() => {
+        addAlert({ content: 'Marca atualizada corretamente!', customSeverity: 'success' });
+        history.push('/usuarios');
+      })
+      .catch(() => {
+        addAlert({ content: 'Erro inesperado ao atualizar marca!', customSeverity: 'error' });
+      })
+      .finally(() => {
+        setIsUpdateLoading(false);
+      });
+  };
+
+  const loadBrand = () => {
+    setIsGetLoading(true);
+    getBrand(brandId, userJWT)
+      .then((data) => {
+        setBrand(data.nome);
+      })
+      .catch(() => {
+        addAlert({
+          content: 'Erro inesperado ao carregar informações da marca!',
+          customSeverity: 'error',
+        });
+        history.push('/usuarios');
+      })
+      .finally(() => {
+        setIsGetLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    if (brandId) {
+      loadBrand();
+    }
+  }, []);
+
+  return { update, isUpdateLoading, isGetLoading };
+};
+
+export default useBrandUpdate;
